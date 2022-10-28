830aa229135e1f2511e68c6c4bb2eb4009436283
Leilen Kalbermatter <leilenkalbermatter@gmail.com>
2022-10-28 00:43:12 -0300

    environment add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   URL: 'https://back-leilenkalbermatter.herokuapp.com/',
+  firebase: {
+    apiKey: "AIzaSyCgB43sKPOmp904or1faRL0Hk_X8A4-IQs",
+    authDomain: "leilenkalbermatter-portf-872a2.firebaseapp.com",
+    projectId: "leilenkalbermatter-portf-872a2",
+    storageBucket: "leilenkalbermatter-portf-872a2.appspot.com",
+    messagingSenderId: "1040345619321",
+    appId: "1:1040345619321:web:772dd424d016df2e6eeda8",
+    measurementId: "G-Y3PS9YD5H3"
+  }
 };
